2d0bf4c4fb1a64fd289c47a5ff8ac5833c57f631
Jonathon Mah <jonathon@playhaus.org>
2006-07-04 05:33:16 -0700

    Created project
    darcs-hash:20060704123316-5716c-9af3198feb4e46ac401e20e05925612137aea296.gz

---
+++ Classes/Controller/INTAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  INTAppDelegate.h
+//  Introspectare
+//
+//  Created by Jonathon Mah on 2006-07-04.
+//  Copyright Playhaus 2006. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface INTAppDelegate : NSObject 
+{
+	IBOutlet NSWindow *window;
+	
+	NSPersistentStoreCoordinator *persistentStoreCoordinator;
+	NSManagedObjectModel *managedObjectModel;
+	NSManagedObjectContext *managedObjectContext;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator;
+- (NSManagedObjectModel *)managedObjectModel;
+- (NSManagedObjectContext *)managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Other Sources/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Introspectare
+//
+//  Created by Jonathon Mah on 2006-07-04.
+//  Copyright Playhaus 2006. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
