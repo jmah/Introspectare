8a0f986fc774d896a91539db71af72383c6df261
Jonathon Mah <jonathon@playhaus.org>
2006-07-24 22:39:01 -0700

    Created INTCircleSwitchButtonCell and used it in place of a regular checkbox in the entries window
    darcs-hash:20060725053901-5716c-64941b767f2d2d702b05440c73a48d0ae58cf30e.gz

---
+++ Classes/View/INTCircleSwitchButtonCell.h
@@ -0,0 +1,17 @@
+//
+//  INTCircleSwitchButtonCell.h
+//  Introspectare
+//
+//  Created by Jonathon Mah on 2006-07-25.
+//  Copyright 2006 Playhaus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface INTCircleSwitchButtonCell : NSButtonCell
+{
+}
+
+
+@end
---
+++ Classes/View/INTCircleSwitchButtonCell.m
@@ -0,0 +1,76 @@
+//
+//  INTCircleSwitchButtonCell.m
+//  Introspectare
+//
+//  Created by Jonathon Mah on 2006-07-25.
+//  Copyright 2006 Playhaus. All rights reserved.
+//
+
+#import "INTCircleSwitchButtonCell.h"
+
+
+#pragma mark Size constants
+
+static const float INTRegularSizeWidth = 8.f;
+static const float INTSmallSizeWidth = 6.f;
+static const float INTMiniSizeWidth = 4.f;
+
+
+
+#pragma mark Helper functions
+
+static NSRect INTMakeCenteredRect(NSRect enclosingRect, NSSize size)
+{
+	NSRect rect = enclosingRect;
+	rect.size = size;
+	rect.origin.x += (NSWidth(enclosingRect) - size.width) / 2.f;
+	rect.origin.y += (NSHeight(enclosingRect) - size.height) / 2.f;
+	return rect;
+}
+
+
+#pragma mark -
+
+
+@implementation INTCircleSwitchButtonCell
+
+#pragma mark Drawing
+
+- (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView // NSCell
+{
+	NSRect circleFrame = INTMakeCenteredRect(cellFrame, [self cellSize]);
+	NSBezierPath *circle = [NSBezierPath bezierPathWithOvalInRect:circleFrame];
+	
+	[NSGraphicsContext saveGraphicsState];
+		[[NSColor grayColor] set];
+		if ([self state] == NSOnState)
+			[circle fill];
+		else
+			[circle stroke];
+	[NSGraphicsContext restoreGraphicsState];
+}
+
+
+
+#pragma mark Determining cell sizes
+
+- (NSSize)cellSize // NSCell
+{
+	NSSize size = NSZeroSize;
+	switch ([self controlSize])
+	{
+		case NSRegularControlSize:
+			size = NSMakeSize(INTRegularSizeWidth, INTRegularSizeWidth);
+			break;
+		case NSSmallControlSize:
+			size = NSMakeSize(INTSmallSizeWidth, INTSmallSizeWidth);
+			break;
+		case NSMiniControlSize:
+			size = NSMakeSize(INTMiniSizeWidth, INTMiniSizeWidth);
+			break;
+	}
+	return size;
+}
+
+
+@end
